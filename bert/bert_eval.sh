5f719384ac14ed4c60bbb6fc85235e7526421207
Dilanga Algama <31867952+dalgama@users.noreply.github.com>
2021-03-20 19:30:06 -0400

    Remove the bert files, small mistake

--- bert/bert_eval.sh
+++
@@ -1,2 +0,0 @@
-## Run the evaluation script.
-../lib/trec_eval ../assets/qrels.txt ../dist/bert_results.txt >> bert_trec_eval.txt
--- bert/bert_full_eval.sh
+++
@@ -1,2 +0,0 @@
-## Run the evaluation script.
-../lib/trec_eval -q ../assets/qrels.txt ../dist/bert_results.txt >> bert_trec_eval_all_query.txt
